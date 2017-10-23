672c674f0c1bcf26fc3b4a19a77ed88df0e4c65e
Amel Dedic <dedica@algonquincollege.com>
2017-10-23 15:02:40 -0400

    updated

--- js/main.js
+++ js/main.js
@@ -27,11 +27,11 @@
         sw = e.currentTarget.innerWidth;
         
         switch (true) {
-            case (sw < 560): link.href = "http://basehold.it/16";
+            case (sw < 560): link.href = "https://basehold.it/16";
                 break;
-            case (sw >= 560) && (sw < 960): link.href = "http://basehold.it/24";
+            case (sw >= 560) && (sw < 960): link.href = "https://basehold.it/24";
                 break;
-            case (sw >= 960): link.href = "http://basehold.it/36";
+            case (sw >= 960): link.href = "https://basehold.it/36";
                 break;
             default: alert("Couldn't read the screen-size!");
         }
